e679ebee6935b664368739065e79a540c07a5bce
nishi18masa@gmail.com <masa18nishi@gmail.com>
2022-04-07 21:26:03 +0900

    seventh commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -23,7 +23,7 @@ class TodoController extends Controller
     function deleteTodo($id){
         $todo = Todo::find($id);
         $todo->delete();
-        return redirect('/todo');
+        return redirect('/todo')->route('todo.index');
     }
 
 }
